c50ad328f2432c3233a7cf8469b86d62fc010dca
Arda <as.ardasen@gmail.com>
2024-06-06 17:47:42 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { useState } from "react";
+
+import "./index.css";
+import App from "./App";
+
+import StartRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StartRating color="blue" maxRating={10} onSetRating={setMovieRating} />;
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StartRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StartRating size={24} color="red" className="test" defaultRating={3} />
+
+    <Test /> */}
+  </React.StrictMode>
+);
